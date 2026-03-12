1aac0aa56b66c2531838958e75e2aa551bcbd0f3
Abinash Mallick (LTIMindtree Limited) <v-abmallick@microsoft.com>
2026-03-12 20:52:06 +0530

    Added python flask app with linter

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from quotes import get_random_quote
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    quote = get_random_quote()
+    return render_template("index.html", quote=quote)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ quotes.py
@@ -0,0 +1,11 @@
+import random
+
+quotes = [
+    "Success is not final, failure is not fatal.",
+    "Believe you can and you're halfway there.",
+    "Dream big and dare to fail.",
+    "Action is the foundational key to success."
+]
+
+def get_random_quote():
+    return random.choice(quotes)
