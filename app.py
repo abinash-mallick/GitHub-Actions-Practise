e69f946baea1e9825b4bbfc6532cdd176674aa7a
Abinash Mallick (LTIMindtree Limited) <v-abmallick@microsoft.com>
2026-03-12 21:21:35 +0530

    Fixed app.py with linter

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+"""
+This module provides functions for performing various arithmetic operations.
+"""
+
 from flask import Flask, render_template
 from quotes import get_random_quote
 
@@ -5,6 +9,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    """ Render the homepage with a random quote """
     quote = get_random_quote()
     return render_template("index.html", quote=quote)
 
